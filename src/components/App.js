52128e15fa79d37c3322301086751a35b7cbd7e8
Brian Sturgis <sturujisu@gmail.com>
2021-07-05 16:14:02 -0700

    add kegcontrol component but does not use it yet

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,17 @@
 import React from "react";
-import KegControl from "./KegControl";
-import './App.css';
+// import KegControl from "./KegControl";
+
 import Header from "./Header";
 
 function App(){
   return (
     <React.Fragment>
+          <Header />
       <div className="container">
         <div className="content">input new keg</div>
         <div className="footer">
           <h3>input form</h3>
-          <KegControl />
-          <Header />
+          {/* <KegControl /> */}
         </div>
       </div>
     </React.Fragment>
