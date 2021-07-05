d51362f023993ea8e5f53f12ca478a69d615da9b
Brian Sturgis <sturujisu@gmail.com>
2021-07-05 15:54:38 -0700

    update project with header, keglist, as well as updates to plac them in app

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
+import React from "react";
+import KegControl from "./KegControl";
 import './App.css';
+import Header from "./Header";
 
 function App(){
   return (
     <React.Fragment>
       <div className="container">
-        <div className="content">keg details</div>
+        <div className="content">input new keg</div>
         <div className="footer">
           <h3>input form</h3>
           <KegControl />
+          <Header />
         </div>
       </div>
     </React.Fragment>
