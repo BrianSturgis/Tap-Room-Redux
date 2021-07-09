9826d5b6914b6114d5215dea0366e8e236cc9521
Brian Sturgis <sturujisu@gmail.com>
2021-07-09 13:59:51 -0700

    this wil fix casing issue with git hub 2

---
+++ src/components/Keg.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+
+
+function Keg(props){
+  return (
+    <React.Fragment>
+      <div onClick = {() => props.whenKegClicked(props.id)}>
+        <h3>{props.brand} - {props.name} </h3>
+        <hr/>
+      </div>
+    </React.Fragment>
+  );
+}
+
+Keg.propTypes = {
+  name: PropTypes.string,
+  brand: PropTypes.string,
+  price: PropTypes.number,
+  alcoholContent: PropTypes.number,
+  pintsLeft: PropTypes.number,
+  id: PropTypes.string,
+  whenKegClicked: PropTypes.func
+};
+export default Keg;
