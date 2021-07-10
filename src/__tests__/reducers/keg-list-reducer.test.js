9031e1322a4e57e1030c67fcfc8a6bf9e20e715e
Brian Sturgis <sturujisu@gmail.com>
2021-07-10 12:34:38 -0700

    be a good fail for adding tickets

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,6 +1,53 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 import * as c from '../../actions/ActionTypes';
 
+
+describe('keg list reducer', () => {
+	let action;
+  const currentState ={
+    1 : {
+      names: 'henry whine herds',
+      brand: 'ipa',
+      price: 350,
+      alcoholContent: 12.0,
+      pintsLeft: 124,
+      id: 1 },
+    2: {
+      names: 'rubinator',
+      brand: 'edgefiled',
+      price: 225,
+      alcoholContent: 9.0,
+      pintsLeft: 100,
+      id: 2,
+    }
+  };
+
 test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
   });
+
+	test('Should successfully add new keg data to masterKegList', () => {
+      const { names, brand, price, alcoholContent, pintsLeft, id } = currentState;
+      action = {
+        type: c.ADD_KEG,
+        names: names,
+        brand: brand,
+        price: price,
+        alcoholContent: alcoholContent,
+        pintsLeft: pintsLeft,
+        id: id
+      };
+
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        names: names,
+        brand: brand,
+        price: price,
+        alcoholContent: alcoholContent,
+        pintsLeft: pintsLeft,
+        id: id
+      }
+    });
+  });
+
+});
