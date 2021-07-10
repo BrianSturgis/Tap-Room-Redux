a0796fe1cf2f682c356c49a494781bdd4b03016b
Brian Sturgis <sturujisu@gmail.com>
2021-07-10 12:39:39 -0700

    be a good pass for adding tickets redcuer

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,4 +1,25 @@
 /* eslint-disable import/no-anonymous-default-export */
+import * as c from "./../actions/ActionTypes";
+
 export default (state = {}, action) => {
+  const { names, brand, price, alcoholContent, pintsLeft, id } = action;
+  switch (action.type) {
+	case c.ADD_KEG:
+		return Object.assign({}, state, {
+			[id]: {
+				names: names,
+				brand: brand,
+				price: price,
+				alcoholContent: alcoholContent,
+				pintsLeft: pintsLeft,
+				id: id
+			}
+		});
+	case c.DELETE_KEG:
+		const newState = { ...state };
+		delete newState[id];
+		return newState;
+	default:
 		return state;
+  }
 };
